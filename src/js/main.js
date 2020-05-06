a0080e63c50cbc6fd0195c895c0bcc72511b6bf8
Flavio <flavio.odas@acct.global>
2020-05-05 21:34:51 -0300

    Corrigindo commits

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {	
+    openMenu();
+    menuColorChange();	
+});
+
+function openMenu(){
+    $('.atena__header-container--menu-button, .atena__close-menu, .atena__overlay').on('click', () =>{
+        $('.atena__mobile-menu, .atena__close-menu, .atena__overlay').toggleClass('is--active');
+    });
+}
+
+function menuColorChange(){
+    $(window).on('scroll', () =>{
+        if ($('.atena__header').offset().top > 0) {
+            $(".atena__header").addClass('has--scrolled');
+        } else {
+            $(".atena__header").removeClass("has--scrolled");
+        }
+    });
+}
 document.addEventListener('mousemove', (e) => {	
 	const sqrs = document.querySelectorAll('.item');
 		
